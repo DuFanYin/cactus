b491ddc1c4df6484d85b952ab024d027c90668cc
Satyajit Kumar <ammesatyajit@gmail.com>
2026-04-13 13:01:25 -0700

    removing repetition penalty fully

--- cactus/graph/graph.h
+++ cactus/graph/graph.h
@@ -635,7 +635,7 @@ public:
 
     size_t sample(size_t logits, float temperature = 0.6f, float top_p = 0.95f, size_t top_k = 20,
                   const std::unordered_map<uint32_t, float>& logit_bias = {});
-    size_t sample_with_options(size_t logits, float temperature, float top_p, float min_p, float repetition_penalty,
+    size_t sample_with_options(size_t logits, float temperature, float top_p, float min_p,
                                size_t top_k, const std::unordered_map<uint32_t, float>& logit_bias = {});
     
     size_t concat(size_t input1, size_t input2, int axis = 0);
